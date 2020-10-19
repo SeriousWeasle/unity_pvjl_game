4d2c97149c4f9871b442ee3e0d155993cf58987b
SeriousWeasle <seriousweasle@gmail.com>
2020-10-19 14:21:55 +0200

    added character controller

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivity = 100f;    //mouse sensitivity
+    public Transform playerBody;    //position and rotation of player
+
+    float sensitivityFactor = 5f;   //multiplication factor for sensitivity
+    float verticalRotation = 0f;    //vertical rotation of camera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hide cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime * sensitivityFactor; //get smoothed x axis and factor in sensitivity and dt
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * sensitivityFactor; //get smoothed y axis and factor in sensitivity and dt
+
+        //calculate y rotation
+        verticalRotation -= mouseY;
+        //prevent head from rotating too far up and/or down
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+        //apply vertical rotation to camera
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        //apply horizontal rotation to player
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Other objects and attributes the script needs
+    public CharacterController controller;  //Player Character controller
+    public Transform groundchecker; //Checker for ground intersections
+    public Transform respawnPoint;
+
+    //public variables
+    public float speed = 7f; //walking speed of character in u/s
+    public float runmult = 2f; //multiplication factor for speed when sprinting
+    public float gravity = 10f; //gravitational acceleration for falling
+    public float groundDistance = 0.5f; //distance the groundchecker will check for ground
+    public float jumpHeight = 2f; //times gravity upwards acceleration in jump
+    public float verticalOOB = -10f; //height at which player will be teleported back because it is out of bounds
+
+    public LayerMask groundMask; //Layer used for jump checking
+
+    //private variables
+    Vector3 movement;
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if player is standing on the ground
+        isGrounded = Physics.CheckSphere(groundchecker.position, groundDistance, groundMask); //Check from the floorchecker with a radius of ground distance for a floor layermask
+
+        //If player reaches ground -> set y velocity to -4 u/s.
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -4f;
+        }
+
+        //get walking inputs
+        float x = Input.GetAxis("Horizontal"); //Strafing
+        float z = Input.GetAxis("Vertical");   //Walking
+
+        //make vector for movement
+        movement = Vector3.ClampMagnitude(transform.right * x + transform.forward * z, 1f); //multiply forward and right vector by movement directions and clamp at 1 unit length
+
+        //check for running
+        if (Input.GetButton("Run"))
+        {
+            movement *= runmult; //if running; multiply the movement vector with the run multiplier
+        }
+
+        //Move character along movement in 1 timestep
+        controller.Move(movement * speed * Time.deltaTime); //move controller with set speed along movement axis, accounting for framerate
+
+        //check if player can jump and is jumping
+        if (isGrounded && Input.GetButton("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * 2f * gravity); //Apply upward force for jump
+        }
+
+        //calculate downward velocity
+        velocity.y += gravity * Time.deltaTime * -1f;
+        controller.Move(velocity * Time.deltaTime);
+
+        //check if player is out of bounds vertically
+        if(groundchecker.transform.position.y < verticalOOB)
+        {
+            gameObject.transform.position = respawnPoint.position;
+        }
+    }
+}
