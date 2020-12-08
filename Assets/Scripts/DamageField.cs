97af973cb5efda037fad837f82033c8abebe1dcd
SeriousWeasle <seriousweasle@gmail.com>
2020-12-08 22:45:16 +0100

    added some stuff

---
+++ Assets/Scripts/DamageField.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class DamageField : MonoBehaviour
+{
+    public float damageDelay = 0.05f;
+    public float damage = 5f;
+
+    PlayerHealth player;
+    float damageTimer = 0f;
+
+    bool containsPlayer = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            containsPlayer = true;
+            player = other.GetComponent<PlayerHealth>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            containsPlayer = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (containsPlayer && damageTimer <= 0)
+        {
+            player.Damage(damage);
+            damageTimer = damageDelay;
+        }
+
+        if (damageTimer > 0)
+        {
+            damageTimer -= Time.deltaTime;
+        }
+    }
+}
--- Assets/Scripts/TestAgent.cs
+++ Assets/Scripts/TestAgent.cs
@@ -6,7 +6,8 @@ public class TestAgent : MonoBehaviour
     //target
     public Transform goal;
     public Transform weapon;
-    public float sightRadius = 5f;
+    public float sightRadius = 20f;
+    public float fireRadius = 10f;
 
     public GameObject projectile;
     public Transform projectilespawn;
@@ -32,7 +33,7 @@ public class TestAgent : MonoBehaviour
             agent.destination = goal.position; //Try to move to player
             gameObject.transform.LookAt(new Vector3(goal.position.x, gameObject.transform.position.y, goal.position.z)); //Face player
             weapon.transform.LookAt(goal.position); //rotate weapon towards center mass of player
-            if (EnemyFireTimer <= 0) //can fire?
+            if (EnemyFireTimer <= 0 && (gameObject.transform.position - goal.transform.position).magnitude < fireRadius) //can fire?
             {
                 GameObject proj = Instantiate(projectile);
                 fireSFX.Play();
