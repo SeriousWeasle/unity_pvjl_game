6606d2b71b705b23b9e394d2e7a9e9cbce7ce0f8
SeriousWeasle <seriousweasle@gmail.com>
2020-12-09 13:07:04 +0100

    added small bit of code

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public Transform toRotate;
+    public Vector3 targetAngle = new Vector3(0, 0, 0);
+
+    public void Rotate()
+    {
+        toRotate.transform.rotation = Quaternion.Euler(targetAngle);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            Rotate();
+        }
+    }
+}
